df99f04f4fc59629c4a9b0967a4c101652ef273f
Sofía Esquivel <sofia.esquivel@lcieducation.net>
2021-09-28 20:54:46 -0600

    Assignment Completed Counter

--- server.py
+++ server.py
@@ -1,19 +1,26 @@
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template, session, redirect
 
-app = Flask( __name__ )
+app = Flask(__name__)
+app.secret_key="verySecret"
 
-@app.route( '/', methods = ['GET'] )
-def eightCheckboard():
-    return render_template( "index.html")
-
-
-
-
-
-
+@app.route( '/', methods=['GET'] )
+def displayCount():
+    if "count" not in session:
+        session["count"] = 0
+    else:
+        session["count"] +=1    
+    return render_template( "index.html" )
 
 
+@app.route('/addTwo', methods=['GET'] )
+def addTwoCount():
+    session["count"] +=1 
+    return redirect('/')
+
+@app.route('/reset', methods=['GET'] )
+def resetCount():
+    session.clear()
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run( debug = True)
